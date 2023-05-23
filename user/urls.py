70cbf6ca3a7ed96357ba91a8726ebc75e3dcebd1
barryjung <wjdgusrbs5@gmail.com>
2023-05-23 15:53:58 +0900

    feat : create token login, refresh, verify urls
    토큰방식 로그인, refresh, verify를 url에 추가했습니다.
    뷰와 시리얼라이저는 제공되는 기능을 사용했습니다.
    (이번엔 payload커스텀은 생략하고자 했습니다)
    jwt세팅은 프로젝트 세팅단계에서 선행된채로 두었습니다.

--- user/urls.py
+++ user/urls.py
@@ -1,6 +1,16 @@
 from django.urls import path
 from user import views
+from rest_framework_simplejwt.views import TokenObtainPairView
+from rest_framework_simplejwt.views import TokenRefreshView
+from rest_framework_simplejwt.views import TokenVerifyView
 
 urlpatterns = [
     path("sign/", views.UserSignView.as_view(), name="sign"),
+    path("token/", TokenObtainPairView.as_view(), name="token"),
+    path("refresh/", TokenRefreshView.as_view(), name="refresh"),
+    # refresh token으로 post요청. 토큰 확인. 유효할시 200. {"access":"<토큰문자열>"}
+    # 유효하지 않을시 401 {"detail":"설명", "code":"token_not_valid"}
+    path("verify/", TokenVerifyView.as_view(), name="verify"),
+    # access token으로 post요청. 토큰이 expire하지 않았는지 확인. 유효할시 200.
+    # 유효하지 않을시 401 {"detail":"설명", "code":"token_not_valid"}
 ]
