ef25f573a3de19126f66b101dd77eee8bf59bc04
2017250297-choi <97ckdtn@gmail.com>
2023-05-27 19:31:38 +0900

    Feat: Change Article - imageField to OnetoOneField
    두 이미지필드를 Picture와 1대1관계로 바꿨습니다.

--- article/models.py
+++ article/models.py
@@ -3,13 +3,13 @@ from user.models import User
 
 
 from user.models import User
+from ai_process.models import Picture
 
 # Create your models here.
 
 
-
 class Article(models.Model):
-  
+
     """Article 모델
 
     ai가 변조할 사진, 유저의 설명, ai의 코멘트, 제목 등 을 담습니다.
@@ -30,8 +30,9 @@ class Article(models.Model):
 
     author = models.ForeignKey(User, on_delete=models.CASCADE)
     title = models.CharField(max_length=30)
-    image = models.ImageField(upload_to="%Y/%m/edited/")
-    origin_image = models.ImageField(upload_to="%Y/%m/origin/")
+    pictures = models.OneToOneField(
+        Picture, on_delete=models.CASCADE, related_name="article"
+    )
     description = models.TextField()
     cat_says = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
@@ -41,27 +42,29 @@ class Article(models.Model):
 
     def __str__(self):
         return str(self.title)
-    
+
+
 class Comment(models.Model):
     """Comment 모델
 
-      아티클에대한 사용자의 짧은 의견을 담은 코멘트 모델입니다.
+    아티클에대한 사용자의 짧은 의견을 담은 코멘트 모델입니다.
 
-      Attributes:
-          author (ForeignKey): 아이디, 외래키, CASCADE
-          article (ForeignKey): 댓글이 달린 아티클, 외래키, CASCADE
-          content (text): 댓글내용
-          created_at (date): 생성일자
-          updated_at (date): 수정일자
+    Attributes:
+        author (ForeignKey): 아이디, 외래키, CASCADE
+        article (ForeignKey): 댓글이 달린 아티클, 외래키, CASCADE
+        content (text): 댓글내용
+        created_at (date): 생성일자
+        updated_at (date): 수정일자
 
     """
-  
+
     author = models.ForeignKey(User, on_delete=models.CASCADE)
-    article = models.ForeignKey(Article, on_delete=models.CASCADE, related_name="comment_set")
+    article = models.ForeignKey(
+        Article, on_delete=models.CASCADE, related_name="comment_set"
+    )
     content = models.TextField(max_length=50)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
-
     def __str__(self):
-       return str(self.content)
+        return str(self.content)
